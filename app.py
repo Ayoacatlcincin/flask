17b4c637bf748cec8de435a4d89bbfc92ba185ae
Ayoacatlcincin Mina Rojas <Mina.Rojas1311@gmail.com>
2020-06-09 15:21:35 -0700

    flask

---
+++ app.py
@@ -0,0 +1,13 @@
+from idea import app, mail
+from flask import render_template, session, request, redirect, url_for, flash, abort
+import os
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+   
+
+if __name__ == '__main__':
+    app.run(debug=False)
